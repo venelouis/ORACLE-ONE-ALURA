c8879867e6f279727eeedfcc2aa1b4caf0f640be
venelouis <venelouistyago@gmail.com>
2025-01-19 21:58:59 -0300

    Add game logic and practice challenges for JavaScript programming

--- 1. Logica de programacao mergulhe com JavaScript/1. logica-js-projeto_inicial/app.js
+++ 1. Logica de programacao mergulhe com JavaScript/1. logica-js-projeto_inicial/app.js
@@ -1,10 +1,11 @@
 alert('Boas vindas ao jogo do número secreto!');
 let numeroSecreto = 5
-
+console.log(numeroSecreto)
 let chute = prompt('Escolha um número secreto entre 1 a 10');
 
+//se o chute for = ao número secreto, exibe mensagem de parabéns
 if (chute == numeroSecreto) {
-    console.log('Parabéns! Você acertou!');
+    alert(`Parabéns! Você acertou o número secreto ${numeroSecreto}!`);
 } else {
     alert('Que pena! O número escolhido foi ' + numeroSecreto);
 }

42ed35018aaccdfaf4c970e5e1a7805766439448
venelouis <venelouistyago@gmail.com>
2025-01-20 00:10:02 -0300

    Enhance game logic to provide feedback on guesses in the number guessing game

--- 1. Logica de programacao mergulhe com JavaScript/1. logica-js-projeto_inicial/app.js
+++ 1. Logica de programacao mergulhe com JavaScript/1. logica-js-projeto_inicial/app.js
@@ -3,9 +3,18 @@ let numeroSecreto = 5
 console.log(numeroSecreto)
 let chute = prompt('Escolha um número secreto entre 1 a 10');
 
+// enquanto
+
+
+
 //se o chute for = ao número secreto, exibe mensagem de parabéns
 if (chute == numeroSecreto) {
     alert(`Parabéns! Você acertou o número secreto ${numeroSecreto}!`);
 } else {
+    if (chute > numeroSecreto) {
+        alert(`O número secreto é menor que ${chute}`);
+    } else {
+        alert(`O número secreto é maior que ${chute}`);
+    }
     alert('Que pena! O número escolhido foi ' + numeroSecreto);
 }

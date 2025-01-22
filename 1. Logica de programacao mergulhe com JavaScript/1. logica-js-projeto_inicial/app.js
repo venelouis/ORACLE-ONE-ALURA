a8a181a1e2a5593b14d5c08774712635b6c4e06e
venelouis <venelouistyago@gmail.com>
2025-01-22 01:13:41 -0300

    Refactor number guessing game logic to improve user input validation and feedback

--- 1. Logica de programacao mergulhe com JavaScript/1. logica-js-projeto_inicial/app.js
+++ 1. Logica de programacao mergulhe com JavaScript/1. logica-js-projeto_inicial/app.js
@@ -1,20 +1,22 @@
 alert('Boas vindas ao jogo do número secreto!');
-let numeroSecreto = 5
-console.log(numeroSecreto)
-let chute = prompt('Escolha um número secreto entre 1 a 10');
+let numeroSecreto = 5;
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
 
 // enquanto
-
-
-
-//se o chute for = ao número secreto, exibe mensagem de parabéns
-if (chute == numeroSecreto) {
-    alert(`Parabéns! Você acertou o número secreto ${numeroSecreto}!`);
-} else {
-    if (chute > numeroSecreto) {
-        alert(`O número secreto é menor que ${chute}`);
+while (chute != numeroSecreto) {
+    alert('Você deve digitar um número entre 1 e 10');
+    chute = prompt('Escolha um número secreto entre 1 a 10');
+    //se o chute for = ao número secreto
+    if (chute == numeroSecreto) {
+        alert(`Parabéns! Você acertou o número secreto ${numeroSecreto} com ${tentativas} tentativas!`);
     } else {
-        alert(`O número secreto é maior que ${chute}`);
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
     }
-    alert('Que pena! O número escolhido foi ' + numeroSecreto);
 }

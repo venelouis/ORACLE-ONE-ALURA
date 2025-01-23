a372d00cc7f2c7dd19439a299d1787ea21318532
venelouis <venelouistyago@gmail.com>
2025-01-23 17:43:37 -0300

    Refactor number guessing game to enhance user input prompts and feedback

--- 1. Logica de programacao mergulhe com JavaScript/1. logica-js-projeto_inicial/app.js
+++ 1. Logica de programacao mergulhe com JavaScript/1. logica-js-projeto_inicial/app.js
@@ -1,22 +1,35 @@
-alert('Boas vindas ao jogo do número secreto!');
-let numeroSecreto = 5;
-console.log(numeroSecreto);
-let chute;
-let tentativas = 1;
+// Exibe uma mensagem de boas-vindas ao jogador
+alert('Boas vindas ao jogo do número secreto');
+let numeroSecreto = parseInt(Math.random() * 100 + 1); // número aleatório entre 1 e 100
+console.log(numeroSecreto); // exibe o número secreto no console
+let chute; // variável para armazenar o chute do jogador
+let tentativas = 1; // variável para armazenar o número de tentativas
 
-
-// enquanto
-while (chute != numeroSecreto) {
-    alert('Você deve digitar um número entre 1 e 10');
-    chute = prompt('Escolha um número secreto entre 1 a 10');
-    //se o chute for = ao número secreto
-    if (chute == numeroSecreto) {
-        alert(`Parabéns! Você acertou o número secreto ${numeroSecreto} com ${tentativas} tentativas!`);
-    } else {
-        if (chute > numeroSecreto) {
-            alert(`O número secreto é menor que ${chute}`);
-        } else {
-            alert(`O número secreto é maior que ${chute}`);
+// enquanto chute não for igual ao n.s.
+while (chute != numeroSecreto) { // enquanto a condição for verdadeira
+    chute = prompt('Escolha um número entre 1 e 100'); // solicita um número ao jogador
+    // se chute for igual ao número secreto
+    if (chute == numeroSecreto) { // se a condição for verdadeira
+        break; // interrompe o loop
+    } else { // se a condição for falsa
+        if (chute > numeroSecreto) { // se a condição for verdadeira
+            alert(`O número secreto é menor que ${chute}`); // exibe uma mensagem
+        } else { 
+            alert(`O número secreto é maior que ${chute}`); // exibe uma mensagem
         }
+        // tentativas = tentativas + 1;
+        tentativas++; // incrementa a variável tentativas
     }
 }
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'; 
+// operador ternário para verificar o número de tentativas e exibir a mensagem correta
+
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`); 
+// exibe uma mensagem com o número secreto e o número de tentativas
+
+// if (tentativas > 1) {
+//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+// } else {
+//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
+// }

43c67aef3b03bb9b4fff31f2a92d2deeb26e9afb
venelouis <venelouistyago@gmail.com>
2025-03-16 16:10:57 -0300

    Add initial game logic and challenges for number guessing game

---
+++ 2. Lógica de programação explore funções e listas/projeto inicial/app.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do número secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Tente adivinhar o número secreto, escolha um número entre 0 e 10 e clique no botão';

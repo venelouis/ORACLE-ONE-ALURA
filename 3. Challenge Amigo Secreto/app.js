d0851403bc40b58fa0937cc5aa3c619c48dd7638
venelouis <venelouistyago@gmail.com>
2025-03-16 17:23:41 -0300

    Add detailed comments and examples for Amigo Secreto challenge in app.js

--- 3. Challenge Amigo Secreto/app.js
+++ 3. Challenge Amigo Secreto/app.js
@@ -1,1 +1,29 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. 
+// Aqui você deverá desenvolver a lógica para resolver o problema.
+
+// Desafio
+// O amigo secreto é uma brincadeira muito comum entre amigos e familiares.
+// Neste desafio, você deverá criar um algoritmo que sorteia um amigo secreto para um grupo de pessoas
+// O seu programa deverá receber um array com o nome de todas as pessoas que irão participar do amigo secreto e sortear a pessoa para cada uma delas.  
+// Cada pessoa deve tirar um amigo secreto e não pode tirar ela mesma. 
+// Ao final o programa deve exibir quem deve presentear quem.
+
+// Entrada
+
+// Saída
+
+// Exemplos
+// Entrada Saída
+// ["A", "B", "C", "D"]
+// A deve presentear C
+// B deve presentear D
+// C deve presentear B
+// D deve presentear A
+
+
+
+
+
+
+
+

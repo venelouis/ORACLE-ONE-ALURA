66f60e07a1b0a724c9ffc75d98c81eab9c2a025c
venelouis <venelouistyago@gmail.com>
2025-04-02 22:26:33 -0300

    Update project configuration and rename main class for clarity

--- 4. Java/1. Java e Orientação a Objetos G8 - ONE/1. Primeiro programa em Java/1. primeiroprojeto/src/Main.java
+++ 4. Java/1. Java e Orientação a Objetos G8 - ONE/1. Primeiro programa em Java/1. primeiroprojeto/src/Main.java
@@ -1,6 +1,6 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class PrimeiraClasse {
+public class Main {
 
     public static void main(String[] args) {
         System.out.print("Concluí a aula 01 e agora estou mergulhando em Java!");

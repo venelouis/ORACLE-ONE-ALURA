d9d56cde2762787e88862672c998eac9217d2366
venelouis <venelouistyago@gmail.com>
2025-04-02 21:57:20 -0300

    Add practical Java exercises to desafio.txt for class concepts reinforcement

---
+++ 4. Java/1. Java e Orientação a Objetos G8 - ONE/1. Primeiro programa em Java/1. primeiroprojeto/src/Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class PrimeiraClasse {
+
+    public static void main(String[] args) {
+        System.out.print("Concluí a aula 01 e agora estou mergulhando em Java!");
+    }
+}

18c7afa441518f18bd37da659d0c67013695dcaa
venelouis <venelouistyago@gmail.com>
2025-04-03 23:39:33 -0300

    Add movie rating average and synopsis to Main class

--- 4. Java/1. Java e Orientação a Objetos G8 - ONE/1. Primeiro programa em Java/1. primeiroprojeto/src/Main.java
+++ 4. Java/1. Java e Orientação a Objetos G8 - ONE/1. Primeiro programa em Java/1. primeiroprojeto/src/Main.java
@@ -10,6 +10,20 @@ public class Main {
         System.out.println("Ano de lançamento: " + anoDeLancamento);
         boolean includioNoPlapo = true;
         double notaDoFilme = 8.1;
+        // médica calculada pelas 3 notas da Jack, Paulo e Suelem
+        double media = (9.8 + 6.3 + 8.0) / 3;
+        System.out.println("Média: " + media);
+
+        String sinopse;
+        
+        sinopse = """
+        Filme Top Gun
+        Filme de aventura com galã dos anos 80
+        Muito bom!
+        Ano de lançamento
+        """ + anoDeLancamento;
+                
+        System.out.println("Sinopse: " + sinopse);
 
     }
 }

99c0a169c4ca113f7310bc263ed9ff06a4510822
venelouis <venelouistyago@gmail.com>
2025-07-27 09:25:19 -0300

    Refactor project structure and add new Java classes for film evaluation and user input handling

--- 4. Java/1. Java e Orientação a Objetos G8 - ONE/1. Java_ criando a sua primeira aplicação/1. primeiroprojeto/src/Main.java
+++ 4. Java/1. Java e Orientação a Objetos G8 - ONE/1. Java_ criando a sua primeira aplicação/1. primeiroprojeto/src/Main.java
@@ -1,32 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Catálogo de Filmes (Minha Netflix)");
-        System.out.println("Filme: Top Gun: Marverick");
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
 
         int anoDeLancamento = 2022;
         System.out.println("Ano de lançamento: " + anoDeLancamento);
-        boolean includioNoPlapo = true;
+        boolean incluidoNoPlano = true;
         double notaDoFilme = 8.1;
-        // médica calculada pelas 3 notas da Jack, Paulo e Suelem
-        double media = (9.8 + 6.3 + 8.0) / 3;
-        System.out.println("Média: " + media);
-
+        //Média calculada pelas 3 notas da Jack, Paulo e Suelem
+        double media = (9.8 + 6.3 + 8.0) /3;
+        System.out.println(media);
         String sinopse;
-        
         sinopse = """
-        Filme Top Gun
-        Filme de aventura com galã dos anos 80
-        Muito bom!
-        Ano de lançamento
-        """ + anoDeLancamento;
-                
-        System.out.println("Sinopse: " + sinopse);
+                Filme Top Gun
+                Filme de aventura com galã dos anos 80
+                Muito bom!
+                Ano de lançamento
+                """ + anoDeLancamento;
+        System.out.println(sinopse);
 
-        int cassificacao = (int) (media / 2);
-        System.out.println("Classificação: " + cassificacao);
-
+        int classificacao = (int) (media /2);
+        System.out.println(classificacao);
     }
 }
